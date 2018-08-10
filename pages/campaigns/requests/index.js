f7fc7c59859b4630681f1b84a1b2e9e0ff7d409e
calvinrzachman <calvin.zachman@protonmail.com>
2018-08-10 16:07:58 -0500

    added link to request list page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -43,6 +43,9 @@ class RequestIndex extends Component {
     const { Cell, Row, Headercell, Body } = Table;
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}`}>
+          <a>Back</a>
+        </Link>
         <h3>Pending Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
